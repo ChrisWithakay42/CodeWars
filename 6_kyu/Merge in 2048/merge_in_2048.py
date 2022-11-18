f5ec74499eb2ecd235168b538e7bb521bda5aa67
ChrisWithakay42 <codewithakay@gmail.com>
2022-11-18 10:03:26 +0000

    test empty grids

--- 6_kyu/Merge in 2048/merge_in_2048.py
+++ 6_kyu/Merge in 2048/merge_in_2048.py
@@ -1,2 +1,8 @@
 class Merge2048:
-    pass
+
+    def merge(self, grid_row: list[int]) -> list[int]:
+        merged_grid_row = []
+        for tile in grid_row:
+            if tile == 0:
+                merged_grid_row.append(tile)
+        return grid_row
--- 6_kyu/Merge in 2048/test_merge_in_2048.py
+++ 6_kyu/Merge in 2048/test_merge_in_2048.py
@@ -1,2 +1,9 @@
+from merge_in_2048 import Merge2048
+
+
 class TestMergeIn2048:
-    pass
+    
+    def test_merge_empty_grid_squares(self):
+        empty_grid_row = [0, 0, 0, 0]
+        assert Merge2048().merge(empty_grid_row) == [0, 0, 0, 0]
+
