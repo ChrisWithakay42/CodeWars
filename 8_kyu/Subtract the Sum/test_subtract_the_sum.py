dab4d51a6d2c6aac453c89a283a62af63720797c
ChrisWithakay42 <codewithakay@gmail.com>
2022-12-29 12:29:19 +0000

    solution + test

--- 8_kyu/Subtract the Sum/test_subtract_the_sum.py
+++ 8_kyu/Subtract the Sum/test_subtract_the_sum.py
@@ -1,6 +1,8 @@
+from subtract_the_sum import subtract_sum
 import pytest
+
 
 class TestSolution:
 
     def test_subtract_sum(self):
-        assert 'apple' == 'apple'
+        assert subtract_sum(42424242) == 'apple'
