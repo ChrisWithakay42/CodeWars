81cacb09028e7d9d20c40ef753175973e75ed046
ChrisWithakay42 <codewithakay@gmail.com>
2023-05-14 11:06:53 +0100

    TDD approach, no google used so far

--- 6_kyu/Ease The Strockbroker/ease_the_stock_broker.py
+++ 6_kyu/Ease The Strockbroker/ease_the_stock_broker.py
@@ -2,4 +2,18 @@ def balance_statement(lst):
     if not lst:
         return 'Buy: 0 Sell: 0'
 
-    return lst.split(', ')
+
+
+def is_valid_order(order):
+    components = order.split()
+    if len(components) != 4:
+        return False
+    quote, quantity, price, status = components
+    try:
+        quantity = int(quantity)
+        price = float(price)
+        if status not in ['B', 'S']:
+            return False
+    except ValueError:
+        return False
+    return True
--- 6_kyu/Ease The Strockbroker/test_ease_the_stock_broker.py
+++ 6_kyu/Ease The Strockbroker/test_ease_the_stock_broker.py
@@ -1,12 +1,27 @@
 import pytest
 from ease_the_stock_broker import balance_statement
-
+from ease_the_stock_broker import is_valid_order
 
 @pytest.fixture
 def orders():
     return 'ZNGA 1300 2.66 B, CLH15.NYM 50 56.32 B,OWW 1000 11.623 B,OGG 20 580.1 B'
-def test_split_orders(orders):
-    assert isinstance(balance_statement(orders), list)
+
+@pytest.fixture
+def badly_formed_orders():
+    return
 
 def test_return_when_empty_order():
     assert balance_statement('') == 'Buy: 0 Sell: 0'
+
+def test_is_valid_order():
+    # Valid orders
+    assert is_valid_order("GOOG 300 542.0 B") == True
+    assert is_valid_order("ZNGA 1300 2.66 B") == True
+    assert is_valid_order("CLH15.NYM 50 56.32 B") == True
+
+    # Invalid orders
+    assert is_valid_order("") == False  # Empty order
+    assert is_valid_order("GOOG 300 542.0 s") == False  # Missing status
+    assert is_valid_order("GOOG 300 542.0 X") == False  # Invalid status
+    assert is_valid_order("GOOG 300 ABC b") == False  # Invalid quantity
+    assert is_valid_order("GOOG 300 542.0 B S") == False  # Extra component
