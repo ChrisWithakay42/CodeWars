719b7c379a157e175e8fa1f013981db2ea3a50e6
ChrisWithakay42 <codewithakay@gmail.com>
2023-05-17 22:29:01 +0100

    doodle

--- 6_kyu/Ease The Strockbroker/ease_the_stock_broker.py
+++ 6_kyu/Ease The Strockbroker/ease_the_stock_broker.py
@@ -1,6 +1,16 @@
 def balance_statement(lst):
     if not lst:
         return 'Buy: 0 Sell: 0'
+    orders = lst.split(', ')
+    valid = []
+    invalid = []
+    for order in orders:
+        if is_valid_order(order):
+            valid.append(order)
+        else:
+            invalid.append(order)
+
+
 
 
 
@@ -10,8 +20,6 @@ def is_valid_order(order):
         return False
     quote, quantity, price, status = components
     try:
-        quantity = int(quantity)
-        price = float(price)
         if status not in ['B', 'S']:
             return False
     except ValueError:
