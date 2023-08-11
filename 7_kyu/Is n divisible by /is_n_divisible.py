ed3c21940b49f31672b2823f02e46c5c95dc778f
ChrisWithakay42 <codewithakay@gmail.com>
2023-08-11 09:19:30 +0100

    refactor

--- 7_kyu/Is n divisible by /is_n_divisible.py
+++ 7_kyu/Is n divisible by /is_n_divisible.py
@@ -1,8 +1,9 @@
 def is_divisible(n: int, *args:int) -> bool:
-    if not args:
-        return True
-
-    for num in args:
-        if n % num != 0:
-            return False
-    return True
+    # if not args:
+    #     return True
+    #
+    # for num in args:
+    #     if n % num != 0:
+    #         return False
+    # return True
+    return all(not n % num for num in args)
