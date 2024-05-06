4470b70153dc7e482da6b03a1c94258c96aaa125
ChrisWithakay42 <codewithakay@gmail.com>
2024-05-06 13:07:42 +0100

    setting up tests

---
+++ Go/8_kyu/TotalAmountOfPoints/total-amount-of-points-test.go
@@ -0,0 +1,12 @@
+package totalamountofpoints
+
+func TestCalculatePoints(t *testing.T) {
+  tests := []struct{
+    testName string
+    matches []string
+    expected int
+  }{
+    {testName: "all wins", matches: []string{"3:1", "3:2", "2:1"}, expected: 9},
+    {testName: "all losses", matches: []string{""}}
+  }
+}
